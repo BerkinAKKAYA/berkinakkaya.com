179c17f1832755ea968f53bbeeb6b8f2e3efb8f9
berkinakkaya <berkin_akkaya@hotmail.com>
2022-05-03 16:59:47 +0300

    compact nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,22 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	typescript: { shim: false },
-	modules: ['@nuxtjs/tailwindcss'],
-	css: ['@/layouts/global.scss'],
-	// vite: {
-	// 	server: {
-	// 		watch: {
-	// 			usePolling: true
-	// 		}
-	// 	},
-	// },
-	layoutTransition: {
-		name: "layout",
-		mode: ""
-	},
-	pageTransition: {
-		name: "default",
-		mode: ""
-	}
+	"typescript": { shim: false },
+	"modules": ['@nuxtjs/tailwindcss'],
+	"css": ['@/layouts/global.scss'],
+	"vite.server.watch.usePolling": true,
 })
