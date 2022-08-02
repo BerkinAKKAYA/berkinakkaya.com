ccecd65d27278fb840a4e0b0ba62ff6d20359277
berkinakkaya <berkin_akkaya@hotmail.com>
2022-08-02 20:17:21 +0300

    better work in progress indicator"

--- pages/work.vue
+++ pages/work.vue
@@ -1,6 +1,14 @@
 <template>
 	<NuxtLayout name="layout">
-		<a href="github.com/berkinakkaya">GitHub</a>
+		<p class="text-center">
+			<span class="opacity-50">This page is a work in progress. For now, you can visit my </span>
+			<b><a href="github.com/berkinakkaya">GitHub</a></b>
+		</p>
+
+		<a href="https://github.com/berkinakkaya" target="_blank" class="mt-10">
+			<VueFeather type="github" stroke="white" size="72" class="draw draw-delay-500"></VueFeather>
+		</a>
+
 		<!--
 		<div>
 			<NuxtLink to="/work?filter=a">a</NuxtLink>
@@ -15,17 +23,20 @@
 </template>
 
 <script>
+import VueFeather from "vue-feather";
+
 export default {
-	data: () => ({
-		filter: "",
-	}),
-	watch: {
-		"$route.query.filter": function (filter) {
-			this.filter = filter;
-		},
-	},
-	created() {
-		this.filter = this.$route.query.filter;
-	},
+	components: { VueFeather },
+	// data: () => ({
+	// 	filter: "",
+	// }),
+	// watch: {
+	// 	"$route.query.filter": function (filter) {
+	// 		this.filter = filter;
+	// 	},
+	// },
+	// created() {
+	// 	this.filter = this.$route.query.filter;
+	// },
 };
 </script>
