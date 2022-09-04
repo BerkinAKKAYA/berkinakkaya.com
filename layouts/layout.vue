92e917938aa57a4f0eac40f9ad688096ad687642
berkinakkaya <berkin_akkaya@hotmail.com>
2022-09-04 12:14:50 +0300

    add footer

--- layouts/layout.vue
+++ layouts/layout.vue
@@ -2,9 +2,11 @@
 	<div>
 		<Header />
 
-		<main class="grid mt-20 mb-32 mx-auto">
+		<main class="grid mt-20 mx-auto">
 			<slot />
 		</main>
+
+		<footer class="text-center my-20">Inspired by <a href="https://brittanychiang.com/" target="_blank">Brittany Chiang</a>'s portfolio.</footer>
 	</div>
 </template>
 
@@ -21,5 +23,13 @@ main {
 	place-items: center;
 	width: 600px;
 	max-width: 90vw;
+}
+
+footer {
+	color: rgba($color: #fff, $alpha: 0.7);
+
+	a {
+		color: #66ffdb;
+	}
 }
 </style>
