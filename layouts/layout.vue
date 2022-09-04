d3b9d0d3a4bf6d6936e7c2f3f05ac62e34282614
berkinakkaya <berkin_akkaya@hotmail.com>
2022-09-04 18:27:24 +0300

    shorten footer animation delay

--- layouts/layout.vue
+++ layouts/layout.vue
@@ -6,7 +6,7 @@
 			<slot />
 		</main>
 
-		<footer class="text-center my-20 reveal" style="animation-delay: 2s">
+		<footer class="text-center my-20 reveal" style="animation-delay: 1s">
 			Inspired by <a href="https://brittanychiang.com/" target="_blank">Brittany Chiang</a>'s portfolio.
 		</footer>
 	</div>
