d5552d6658bc5daea3329a3a518d705a75eaf15b
Berkin AKKAYA <berkin_akkaya@hotmail.com>
2023-12-08 17:57:54 +0300

    add phone number

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,6 +3,7 @@
 
 	<div>
 		<p><b>Mail:</b> <a href="mailto:berkin_akkaya@hotmail.com" class="link">berkin_akkaya@hotmail.com</a></p>
+		<p><b>Phone:</b> <a href="tel:+905382848787" class="link">+90 538 284 8787</a></p>
 	</div>
 </template>
 
