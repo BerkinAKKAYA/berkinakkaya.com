1a3fb19ba797b67328a5d046ff59e5e9dc59a6ab
Berkin AKKAYA <berkin_akkaya@hotmail.com>
2023-12-08 17:59:51 +0300

    update phone location

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,6 +3,9 @@
 
 	<div>
 		<p><b>Mail:</b> <a href="mailto:berkin_akkaya@hotmail.com" class="link">berkin_akkaya@hotmail.com</a></p>
+	</div>
+	
+	<div>
 		<p><b>Phone:</b> <a href="tel:+905382848787" class="link">+90 538 284 8787</a></p>
 	</div>
 </template>
