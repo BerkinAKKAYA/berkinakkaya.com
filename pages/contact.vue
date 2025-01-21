3369c1c41eb16e83e4e0cf4bf68938cb22b3742c
Berkin Akkaya <berkin_akkaya@hotmail.com>
2025-01-21 20:31:36 +0300

    format and add linkedin to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,14 +2,28 @@
 	<Head><Title>Contact | Berkin Akkaya</Title></Head>
 
 	<div>
-		<p><b>Mail:</b> <a href="mailto:berkin_akkaya@hotmail.com" class="link">berkin_akkaya@hotmail.com</a></p>
-	</div>
-	
-	<div>
-		<p><b>Phone:</b> <a href="tel:+905382848787" class="link">+90 538 284 8787</a></p>
+		<div class="link-container">
+			<p><b>Mail:</b> <a href="mailto:berkin_akkaya@hotmail.com" class="link">berkin_akkaya@hotmail.com</a></p>
+		</div>
+
+		<div class="link-container">
+			<p><b>Phone:</b> <a href="tel:+905382848787" class="link">+90 538 284 8787</a></p>
+		</div>
+
+		<div class="link-container">
+			<p><b>LinkedIn:</b> <a href="https://www.linkedin.com/in/berkinakkaya" class="link">linkedin.com/in/berkinakkaya </a></p>
+		</div>
 	</div>
 </template>
 
 <script>
 definePageMeta({ layout: "main" });
 </script>
+
+<style>
+.link-container p b {
+	display: inline-block;
+	width: 100px;
+	text-align: right;
+}
+</style>
